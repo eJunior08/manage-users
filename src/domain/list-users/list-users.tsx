94cdd8c45ec0d2cf24c0e26b01ec20a36b199072
Elson Oliveira <elsonfojr@gmail.com>
2022-02-02 23:21:09 -0300

    List users screen created

---
+++ src/domain/list-users/list-users.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {FlatList} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+/* Components */
+import {EmptyList} from '@domain/list-users/components/empty-list';
+
+/* Model */
+import {User} from '@models/user';
+
+import * as S from './styles';
+
+export function ListUsers() {
+  const navigation = useNavigation();
+
+  const items: User[] = [
+    /* {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''},
+    {code: '', birthdate: '', id: '', name: ''}, */
+  ];
+
+  const renderItem = () => {
+    const route = 'UserDetail' as never; /* Issue React Navigation */
+    return <S.UserCard onPress={() => navigation.navigate(route)} />;
+  };
+
+  return (
+    <S.Wrapper>
+      <FlatList
+        data={items}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={renderItem}
+        ListEmptyComponent={<EmptyList />}
+      />
+    </S.Wrapper>
+  );
+}
---
+++ src/domain/list-users/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+import {Card} from '@domain/list-users/components/card';
+
+export const Wrapper = styled.View`
+  flex: 1;
+  padding: 0 16px;
+`;
+
+export const UserCard = styled(Card)`
+  margin-bottom: 10px;
+`;
