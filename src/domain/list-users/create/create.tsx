2f88cd8f69daea22e43f089799bddc9996774622
Elson Oliveira <elsonfojr@gmail.com>
2022-02-03 12:36:25 -0300

    Elements added to create screen

--- src/domain/list-users/create/create.tsx
+++ src/domain/list-users/create/create.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import {useNavigation} from '@react-navigation/native';
+
 import * as S from './styles';
 
 export function Create() {
-  return <S.Wrapper />;
+  const navigation = useNavigation();
+
+  function onPressProfile() {
+    const route = 'TakePhoto' as never;
+    navigation.navigate(route);
+  }
+
+  return (
+    <S.Wrapper>
+      <S.ProfileContainer onPress={onPressProfile} />
+
+      <S.Input placeholder="Insira o código" />
+      <S.Input placeholder="Insira o nome" />
+      <S.Input placeholder="Insira a data de nascimento" />
+    </S.Wrapper>
+  );
 }
--- src/domain/list-users/create/styles.ts
+++ src/domain/list-users/create/styles.ts
@@ -1,6 +1,22 @@
 import styled from 'styled-components/native';
 
+import {metric} from '@theme/metrics';
+
 export const Wrapper = styled.View`
   flex: 1;
   background-color: green;
+  padding-top: ${metric.statusBarHeight}px;
+`;
+
+export const ProfileContainer = styled.Pressable`
+  width: 200px;
+  height: 200px;
+
+  border-radius: 100px;
+
+  background-color: blue;
+`;
+
+export const Input = styled.TextInput`
+  border-bottom-width: 1px;
 `;
