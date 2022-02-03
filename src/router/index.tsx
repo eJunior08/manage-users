e88f4f6fdad046437ff7d705ec3c084f429d89d0
Elson Oliveira <elsonfojr@gmail.com>
2022-02-02 23:20:51 -0300

    Detail screen added to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {ListUsers} from '@domain/list-users';
+import {Detail} from '@domain/list-users/detail';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export function Routes() {
         initialRouteName="ListUsers"
         screenOptions={{headerShown: false}}>
         <Screen name="ListUsers" component={ListUsers} />
+        <Screen name="UserDetail" component={Detail} />
       </Navigator>
     </NavigationContainer>
   );
