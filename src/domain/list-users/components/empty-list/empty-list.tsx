6be42a44432f6b744691153a2eaeeecf7e22820d
Elson Oliveira <elsonfojr@gmail.com>
2022-02-04 11:23:38 -0300

    Icon added to empty list component

--- src/domain/list-users/components/empty-list/empty-list.tsx
+++ src/domain/list-users/components/empty-list/empty-list.tsx
@@ -5,6 +5,7 @@ import * as S from './styles';
 export function EmptyList() {
   return (
     <S.Wrapper>
+      <S.StyledIcon />
       <S.Title>Nenhum usuário cadastrado</S.Title>
       <S.Title>Pressione o botão para cadastrar</S.Title>
     </S.Wrapper>
--- src/domain/list-users/components/empty-list/styles.ts
+++ src/domain/list-users/components/empty-list/styles.ts
@@ -1,15 +1,24 @@
 import styled from 'styled-components/native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {Dimensions} from 'react-native';
+import {color} from '@theme/color';
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export const Wrapper = styled.View`
-  height: ${SCREEN_HEIGHT}px;
+  height: ${SCREEN_HEIGHT - 200}px;
   align-items: center;
   justify-content: center;
 `;
 
 export const Title = styled.Text`
-  color: black;
+  color: ${({theme}) => theme.color.gray9};
   font-family: 'Poppins-Regular';
 `;
+
+export const StyledIcon = styled(Icon).attrs({
+  name: 'list',
+  color: color.gray8,
+  size: 75,
+})``;
