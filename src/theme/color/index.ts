0aa437323a7b65b4fa71bfa06f6709d87943d608
Elson Oliveira <elsonfojr@gmail.com>
2022-02-04 10:14:14 -0300

    Colors added

--- src/theme/color/index.ts
+++ src/theme/color/index.ts
@@ -1,4 +1,18 @@
 export const color = {
   primary: '#1D3557',
   light: '#FFFFFF',
+
+  placeholderColor: '#CCC',
+
+  gray1: '#f8f9fa',
+  gray2: '#e9ecef',
+  gray3: '#dee2e6',
+  gray4: '#ced4da',
+  gray5: '#adb5bd',
+  gray6: '#6c757d',
+  gray7: '#495057',
+  gray8: '#343a40',
+  gray9: '#212529',
+
+  backgroundColor: '#edf2f4',
 };
--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -7,6 +7,20 @@ declare module 'styled-components' {
     color: {
       primary: string;
       light: string;
+
+      placeholderColor: string;
+
+      backgroundColor: string;
+
+      gray1: string;
+      gray2: string;
+      gray3: string;
+      gray4: string;
+      gray5: string;
+      gray6: string;
+      gray7: string;
+      gray8: string;
+      gray9: string;
     };
   }
 }
