0b289f3e5ca6ac7bec514dfde98f435fd7f96682
Elson Oliveira <elsonfojr@gmail.com>
2022-02-04 11:23:12 -0300

    Creating user on firebase

--- src/domain/list-users/create/create.tsx
+++ src/domain/list-users/create/create.tsx
@@ -1,24 +1,38 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
+
+import _isUndefined from 'lodash/isUndefined';
+import _isEmpty from 'lodash/isEmpty';
 
 import database from '@react-native-firebase/database';
+import storage from '@react-native-firebase/storage';
 
 import {useNavigation} from '@react-navigation/native';
 
 import * as S from './styles';
 import {User} from '@models/user';
+import ProfileContext from '@contexts/profile';
 
-export function Create() {
+export function Create({route}: any) {
+  const user = route?.params?.item as User;
+  console.log({user});
+  const isUpdating = !_isUndefined(user);
+
+  const {profileUri, setProfileUri} = useContext(ProfileContext);
+
+  const imageUri = _isEmpty(profileUri) ? user?.imageUri ?? '' : profileUri;
+  console.log({imageUri});
+
   const navigation = useNavigation();
 
-  const [payload, setPayload] = useState<User>({
-    code: '',
-    name: '',
-    birthdate: '',
+  const [payload, setPayload] = useState<Omit<User, 'id' | 'imageUri'>>({
+    code: isUpdating ? user.code : '',
+    name: isUpdating ? user.name : '',
+    birthdate: isUpdating ? user.birthdate : '',
   });
 
   function onPressProfile() {
-    const route = 'TakePhoto' as never;
-    navigation.navigate(route);
+    const routeName = 'TakePhoto' as never;
+    navigation.navigate(routeName);
   }
 
   function onChangeText(text: string, field: string) {
@@ -28,7 +42,18 @@ export function Create() {
   async function onSave() {
     try {
       const newReference = database().ref('/users').push();
-      await newReference.set({...payload, createdAt: new Date().getTime()});
+      await newReference.set({
+        ...payload,
+        id: newReference.key,
+        createdAt: new Date().getTime(),
+      });
+
+      const reference = storage().ref(newReference.key as string);
+      const pathToFile = profileUri;
+      await reference.putFile(pathToFile as string);
+
+      setProfileUri('');
+
       navigation.goBack();
     } catch (error) {
       const message = 'Erro ao tentar salvar';
@@ -38,23 +63,31 @@ export function Create() {
 
   return (
     <S.Wrapper>
-      <S.ProfileContainer onPress={onPressProfile} />
+      <S.ProfileContainer onPress={onPressProfile}>
+        {!_isEmpty(imageUri) && <S.Image source={{uri: imageUri}} />}
 
-      <S.Input
-        placeholder="Insira o código"
-        value={payload.code}
-        onChangeText={text => onChangeText(text, 'code')}
-      />
-      <S.Input
-        placeholder="Insira o nome"
-        value={payload.name}
-        onChangeText={text => onChangeText(text, 'name')}
-      />
-      <S.Input
-        placeholder="Insira a data de nascimento"
-        value={payload.birthdate}
-        onChangeText={text => onChangeText(text, 'birthdate')}
-      />
+        <S.CameraContainer>
+          <S.CameraIcon name="photo-camera" size={27.5} color="#fff" />
+        </S.CameraContainer>
+      </S.ProfileContainer>
+
+      <S.InputContainer>
+        <S.Input
+          placeholder="Insira o código"
+          value={payload.code}
+          onChangeText={text => onChangeText(text, 'code')}
+        />
+        <S.Input
+          placeholder="Insira o nome"
+          value={payload.name}
+          onChangeText={text => onChangeText(text, 'name')}
+        />
+        <S.Input
+          placeholder="Insira a data de nascimento"
+          value={payload.birthdate}
+          onChangeText={text => onChangeText(text, 'birthdate')}
+        />
+      </S.InputContainer>
 
       <S.Button onPress={onSave} />
     </S.Wrapper>
--- src/domain/list-users/create/styles.ts
+++ src/domain/list-users/create/styles.ts
@@ -1,11 +1,16 @@
 import styled from 'styled-components/native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {metric} from '@theme/metrics';
+import {color} from '@theme/color';
 
 export const Wrapper = styled.View`
   flex: 1;
-  background-color: green;
+  background-color: ${({theme}) => theme.color.backgroundColor};
   padding-top: ${metric.statusBarHeight}px;
+
+  justify-content: space-evenly;
 `;
 
 export const ProfileContainer = styled.Pressable`
@@ -14,13 +19,54 @@ export const ProfileContainer = styled.Pressable`
 
   border-radius: 100px;
 
-  background-color: blue;
+  background-color: ${({theme}) => theme.color.gray3};
 
   align-self: center;
+
+  position: relative;
+
+  margin-bottom: 50px;
 `;
 
-export const Input = styled.TextInput`
+export const Image = styled.Image.attrs({
+  resizeMode: 'cover',
+})`
+  width: 200px;
+  height: 200px;
+
+  border-radius: 100px;
+`;
+
+export const CameraContainer = styled.View`
+  width: 55px;
+  height: 55px;
+
+  border-radius: 27.5px;
+
+  background-color: ${({theme}) => theme.color.gray4};
+
+  position: absolute;
+
+  bottom: -25px;
+  align-self: center;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CameraIcon = styled(Icon)``;
+
+export const InputContainer = styled.View`
+  /* flex: 1; */
+  height: 200px;
+  justify-content: space-between;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: color.placeholderColor,
+})`
   border-bottom-width: 1px;
+  color: red;
 `;
 
 export const Button = styled.Pressable`
